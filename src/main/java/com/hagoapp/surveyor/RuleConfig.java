2f7ad0a2ab50d4c8f735b99b17eddc09d403dd0b
Chaojun Sun <suncj2@lenovo.com>
2022-04-28 16:03:23 +0800

    ...

--- src/main/java/com/hagoapp/surveyor/RuleConfig.java
+++ src/main/java/com/hagoapp/surveyor/RuleConfig.java
@@ -11,26 +11,12 @@ import java.util.List;
 
 public class RuleConfig {
     private String configType;
-    private List<Object> parameters;
-    private int paramCount = -1;
 
     public String getConfigType() {
         return configType;
     }
 
-    public List<Object> getParameters() {
-        return parameters;
-    }
-
-    public void setParameters(List<Object> parameters) {
-        this.parameters = parameters;
-    }
-
     public int getParamCount() {
-        return paramCount;
-    }
-
-    public void setParamCount(int paramCount) {
-        this.paramCount = paramCount;
+        return -1;
     }
 }
---
+++ src/main/java/com/hagoapp/surveyor/rule/RegexRuleConfig.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2021.
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.surveyor.rule;
+
+import com.hagoapp.surveyor.RuleConfig;
+
+public class RegexRuleConfig extends RuleConfig {
+    private String pattern;
+    private boolean caseSensitive;
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public boolean isCaseSensitive() {
+        return caseSensitive;
+    }
+
+    public void setCaseSensitive(boolean caseSensitive) {
+        this.caseSensitive = caseSensitive;
+    }
+
+    @Override
+    public int getParamCount() {
+        return 1;
+    }
+}
