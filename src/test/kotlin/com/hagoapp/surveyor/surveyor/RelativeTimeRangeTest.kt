63521f8974a367d6e252677264a12162afe0b75c
Chaojun Sun <suncjs@lenovo.com>
2022-12-21 09:33:13 +0800

    add a test and passed

--- src/test/kotlin/com/hagoapp/surveyor/surveyor/RelativeTimeRangeTest.kt
+++ src/test/kotlin/com/hagoapp/surveyor/surveyor/RelativeTimeRangeTest.kt
@@ -56,4 +56,27 @@ class RelativeTimeRangeTest {
             Assertions.assertTrue(result)
         }
     }
+
+    @Test
+    fun testWithinOneWeekAgoAndOneWeekLater() {
+        val nowMilli = Instant.now().toEpochMilli()
+        val random = Random(nowMilli)
+        val sometimeYesterday = nowMilli + random.nextInt(86400 * 14) * (if (random.nextBoolean()) 1 else -1)
+        val config = RelativeTimeRangeConfig(
+            RelativeTimeBoundary(
+                TimeAnchor.BeginOfThisWeek, TimeDiff(
+                    0, 0, random.nextInt(-7, 0), 0, 0, 0, false
+                ), true
+            ),
+            RelativeTimeBoundary(
+                TimeAnchor.BeginOfThisWeek, TimeDiff(
+                    0, 0, random.nextInt(7), 0, 0, 0, true
+                ), true
+            )
+        )
+        SurveyorFactory.createSurveyor(config).use { surveyor ->
+            val result = surveyor.process(sometimeYesterday)
+            Assertions.assertTrue(result)
+        }
+    }
 }
